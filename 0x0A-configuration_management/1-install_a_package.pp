c38117ba29f01891f234e6df02b696d3a5259266
courte_labs <courte_labs@kali.courte_labs>
2022-09-16 12:12:09 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#install flask package
-
-package{ 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'pip',
+# Installs puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
+
