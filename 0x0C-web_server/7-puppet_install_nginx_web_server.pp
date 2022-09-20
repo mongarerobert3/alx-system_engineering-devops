07fe869551ab1eafdb98971ebb891ddf4a4beb39
courte_labs <courte_labs@kali.courte_labs>
2022-09-20 18:21:02 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,6 @@
-# Install and configure an Nginx server with the following requirements:
-# Listens on port 80
-# When querying Nginx at its root /, return a page containing the string Holberton School
-# Perform a permanent redirect when you query Nginx at /redirect_me
+# Installs a Nginx server
 
-package { 'nginx':
-      ensure => installed,
-}
-
-file_line { 'rewrite redirect':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'server_name _;',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-      content => 'Hello World!',
-      require => Package['nginx'],
-}
-
-service { 'nginx':
-    ensure  => 'running',
-    require => file_line['rewrite redirect'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/adamkeino permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
